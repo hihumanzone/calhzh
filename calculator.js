1912731eae134adb7eb89ef96d481fa7ca4436bc
hihumanzone <101509991+hihumanzone@users.noreply.github.com>
2023-09-05 15:49:10 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -5,7 +5,7 @@ window.onload = function() {
             var result = document.getElementById('result');
             if (button.textContent === 'C') {
                 result.textContent = '0';
-            } else if (button.textContent === '«') {
+            } else if (button.textContent === '<') {
                 result.textContent = result.textContent.slice(0, -1);
                 if (result.textContent === '') {
                     result.textContent = '0';
